081f5fe9818ffcae12902d4d22e6d90c1a139c90
sihabrahman <sihabrahman@gmail.com>
2021-12-18 17:08:10 +0600

    Static Method

---
+++ StaticMethod/static_method.java
@@ -0,0 +1,14 @@
+
+package StaticMethod;
+
+public class static_method {
+    
+    void display1(){
+        System.out.println("Non Static Method");
+    }
+    
+    static void display2(){
+        System.out.println("Static Method");
+    }
+    
+}
---
+++ StaticMethod/test.java
@@ -0,0 +1,14 @@
+
+package StaticMethod;
+
+public class test {
+    
+    public static void main(String[] args) {
+        
+        static_method ob1 = new static_method();
+        ob1.display1();
+        
+        static_method.display2();
+    }
+    
+}
