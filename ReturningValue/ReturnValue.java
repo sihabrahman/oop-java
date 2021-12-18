0a4b68aebe61055d6bd524962eaafebdd8478769
sihabrahman <sihabrahman@gmail.com>
2021-12-18 17:07:38 +0600

    Return Value

---
+++ ReturningValue/ReturnValue.java
@@ -0,0 +1,11 @@
+
+package ReturningValue;
+
+public class ReturnValue {
+    
+    //creating a method
+    int square(int a){
+        //return value
+        return a*a;
+    }
+}
---
+++ ReturningValue/test.java
@@ -0,0 +1,11 @@
+
+package ReturningValue;
+
+public class test {
+    public static void main(String[] args) {
+        
+        //creating a new object x under the class ReturnValue
+        ReturnValue x = new ReturnValue();
+        System.out.println(x.square(10));
+    }
+}
